9eb54575370743fa89ecac09428e3e006e4be014
shuhei.nishijima <nishijimasyuhei@gmail.com>
2021-06-22 01:23:12 +0900

    file changed

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,17 +1,36 @@
 <template>
-  <div>
-    <c-button>Button</c-button>
+  <div class="index-main">
+    <ul class="choices-list">
+      <li v-for="(item, index) in items[0].choices" :key="index" class="choices-list__item">
+        <c-button>{{ item }}</c-button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import CButton from '@/components/CButton/CButton.vue'
 
 export default {
   name: 'Index',
   components: {
     CButton,
+  },
+  data() {
+    return {
+      count: 0,
+      items: [],
+    }
+  },
+  created() {
+    this.getApiData()
+  },
+  methods: {
+    async getApiData() {
+      const REQUEST_URL = '/json/quiz.json'
+      const response = await this.axios(REQUEST_URL)
+      this.items = response.data.quiz[0].items
+    },
   },
 }
 </script>
